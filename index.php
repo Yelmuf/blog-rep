b672d1200a987c1a49cd023b17ec92514663153b
kaffka <kaffkasama@ya.ru>
2016-08-12 22:18:15 +0300

    lesson 4 done

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
-echo "Hello world!";
+//echo "Hello world!";
+//echo time();
+$a = $_GET['a'];
+$b = $_GET['b'];
 
-
-
+function add($param1,$param2){
+    return $param1+$param2;
+}
+echo add($a,$b);
+echo "<br>";
+for($i=0;$i<10;$i++){
+    echo $i."<br>";
+}
 ?>
